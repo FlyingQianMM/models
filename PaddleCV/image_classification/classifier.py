5b4d66f2eada677c9c299c8242b342baa56d1819
jiangjiajun <jiangjiajun@baidu.com>
2019-11-01 16:30:43 +0800

    little modify

--- PaddleCV/image_classification/classifier.py
+++ PaddleCV/image_classification/classifier.py
@@ -15,7 +15,6 @@ def path_join(dir_path, file_or_dir):
 def merge_cfg(**kwargs):
     args = parse_args()
     for k, v in kwargs.items():
-        print("=====", k, v)
         if hasattr(args, k):
             setattr(args, k, v)
     return args
@@ -25,11 +24,15 @@ class Classifier(object):
     def __init__(self,
                  work_dir,
                  model_name,
+                 num_classes=None,
                  use_pretrained_weights=False,
                  use_gpu=True):
         self.model_name = model_name
         self.work_dir = work_dir
         self.use_pretrained_weights = use_pretrained_weights
+        if num_classes is None:
+            raise Exception("num_classes must be defined in Classifier")
+        self.class_dim = num_classes
         self.use_gpu = use_gpu
         self.exe = None
         if use_pretrained_weights:
@@ -37,17 +40,16 @@ class Classifier(object):
             self.pretrained_weights_dir = get_pretrained_weights(model_name,
                                                                  pretrain_dir)
 
-    def fit(self, data_dir, num_epochs=120, lr=0.1, class_dim=None):
+    def fit(self, data_dir, num_epochs=120, lr=0.1, batch_size=8):
         cfg = merge_cfg(**locals())
+        cfg.class_dim = self.class_dim
         self.model_save_dir = path_join(self.work_dir, "saved_model")
         if self.use_pretrained_weights:
             cfg.pretrained_model = self.pretrained_weights_dir
             remove_fc_vars(self.pretrained_weights_dir, self.model_name)
         cfg.model = self.model_name
         cfg.use_gpu = self.use_gpu
-        cfg.class_dim = class_dim
         self.cfg = cfg
-        self.class_dim = class_dim
         cfg.model_save_dir = osp.join(self.work_dir, 'output')
         train_txt_path = osp.join(data_dir, 'train_list.txt')
         assert osp.exists(train_txt_path), \
@@ -78,9 +80,7 @@ class Classifier(object):
         eval(cfg, self.train_res[0], self.train_res[1], self.train_res[2],
              self.train_res[3])
 
-    def load_model(
-            self,
-            model_dir, ):
+    def load_model(self, model_dir):
         if self.exe is None:
             place = fluid.CUDAPlace(0) if self.use_gpu else fluid.CPUPlace()
             exe = fluid.Executor(place)
@@ -90,11 +90,11 @@ class Classifier(object):
 
 
 #    def save_inference_model(self):
-mymodel = Classifier(
-    work_dir="myproject", model_name="ResNet18", use_pretrained_weights=True)
+mymodel = Classifier(work_dir="myproject", model_name="ResNet18", use_pretrained_weights=True, num_classes=114)
 print(mymodel.pretrained_weights_dir)
-mymodel.fit(data_dir="../mini_data", num_epochs=1, lr=0.1, class_dim=283)
-print('________________________________eval__________________________')
-mymodel.eval(data_dir="../mini_data")
+mymodel.fit(data_dir="/ssd3/jiangxiaobai/data0926", num_epochs=2, lr=0.05, batch_size=32)
+
+print("===============eval=============================")
+mymodel.eval(data_dir="/ssd3/jiangxiaobai/data0926")
 print('________________________________infer__________________________')
 mymodel.predict(img_file='/all/mini_data/kmeans_data/0/9.jpg')
